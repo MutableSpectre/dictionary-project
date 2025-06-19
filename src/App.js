f5c4765d9d776451736e1a1633a29cf1bc6d6204
Stephanie Marie <76826555+MutableSpectre@users.noreply.github.com>
2025-06-19 13:19:16 -0400

    Added div and footer

--- src/App.js
+++ src/App.js
@@ -4,9 +4,14 @@ import './App.css';
 export default function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo img-fluid" alt="logo" />
-      </header>
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo img-fluid" alt="logo" />
+        </header>
+        <footer className="text-center">
+          Coded by Stephanie Ratliff-Putney
+        </footer>
+      </div>
     </div>
   );
 }
