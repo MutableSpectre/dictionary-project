1b34a7c11a91c420bc9ebe2270e98fafbb66ac5f
Stephanie Marie <76826555+MutableSpectre@users.noreply.github.com>
2025-06-19 08:01:20 -0400

    Changed Title and Removed Links

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
+import logo from "./logo.png"
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header"></header>
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+      </header>
     </div>
   );
 }
