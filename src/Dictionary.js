a4714629d92ffb57ce7b97f339025b58dcb72f93
Stephanie Marie <76826555+MutableSpectre@users.noreply.github.com>
2025-06-23 21:20:17 -0400

    SheCodes' Dictionary API Integration
    I frickin' did it! I figured out how to add the SheCodes version instead. Woot! Challenges are fun!

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -5,19 +5,23 @@ import "./Dictionary.css";
 export default function Dictionary() {
     let [keyword, setKeyword] = useState("");
 
-    function handleKewordChange(event) {
-        setKeyword(event.target.value);
-
+    function handleResponse(response) {
+        console.log(response.data[0]);
     }
 
     function search(event) {
         event.preventDefault();
-        alert(`Searching for ${keyword}`);
-
-        let apiWord = "book";
-        let apiKey = "40e28c77t4bb5368od54b9ccc0f6fad9";
-        let apiUrl = `https://api.shecodes.io/dictionary/v1/define?word={apiWord}&key={apiKey}`;
+        alert(`Searching for ${keyword} definition`);
+        
+        let word = `${keyword}`
+        let apiKey = "40e28c77t4bb5368od54b9ccc0f6fad9"
+        let apiUrl = `https://api.shecodes.io/dictionary/v1/define?word=${word}&key=${apiKey}`;
         axios.get(apiUrl).then(handleResponse)
+    }
+
+    function handleKewordChange(event) {
+        setKeyword(event.target.value);
+
     }
 
     return (
